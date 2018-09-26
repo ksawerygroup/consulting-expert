35fa9d23290c86546187919146d780266475f57a
Ksawery <ksawerygroup@gmail.com>
2018-09-26 15:05:02 +0200

    add file directory

--- script/script.js
+++ script/script.js
@@ -29,3 +29,22 @@ $('.navbar-nav a, .top-button').on('click', function () { //pobieramy przycisk a
   }, 500)
 
 })
+
+// 
+
+//                                
+
+$(document).on('scroll', function () {
+  const windowHeight = $(window).height();
+  const scrollValue = $(this).scrollTop();
+  const $art1 = $('.art-introduction');
+  const art1FromTop = $art1.offset().top; // pobieramy gdzie ten element jest od początku układu
+  const art1Height = $art1.outerHeight(); // outerHeight pobranie wysokości elementu z padingiem i borderem
+
+  if (scrollValue > art1FromTop + art1Height - windowHeight) {
+    $art1.addClass('active');
+  }
+  if (scrollValue < 100) { //jeśli wysokość scrolla jest mniejsza niż 100 px Pamiętaj nie piszemy PX to zabierz klasę active
+    $('.art-introduction').removeClass('active');
+  }
+})
